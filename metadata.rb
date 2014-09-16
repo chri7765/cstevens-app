afaa3742f154bb179c9ce11ac8d06fa2677907c8
Christopher Stevens <christopher.stevens@rackspace.com>
2014-09-16 10:01:24 -0500

    Berks init

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name 'cstevens-app'
+maintainer 'Rackspace'
+license 'All rights reserved'
+description 'Installs/Configures cstevens-app'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.0.1'
