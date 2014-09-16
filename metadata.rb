8152dd0a6bb491e82bc191415f916ace8a26fc47
Christopher Stevens <christopher.stevens@rackspace.com>
2014-09-16 10:49:12 -0500

    updated cookbook depends

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,7 @@ license 'All rights reserved'
 description 'Installs/Configures cstevens-app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.0.2'
+
+depends 'platformstack'
+depends 'rackops_rolebook'
+depends 'pythonstack::application_python'
